e81c90db0767e3df81a890f2167cb8029ea05f89
Cicero <manuelchirilli@gmail.com>
2025-12-06 12:46:53 +0100

    add fornti fotoer

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -5,8 +5,11 @@ export default function Footer() {
   return (
 
     <footer>
-        <p>&copy; 2025 <a href='https://github.com/uCiceroCODE/GenderGap'>uCiceroCODE</a> | NULL. Analisi basata su [mettiamo le fonti]</p>
-        <p style={{fontSize:"0.85rem", marginTop:"1rem"}}>Progetto realizzato con React, Vite, Three.js | Dati aggiornati al 2025</p>
+        <p>&copy; 2025 <a href='https://github.com/uCiceroCODE/GenderGap'>uCiceroCODE</a> | TEAM NULL. Analisi basata su dati estratti da:{"  "}
+        <a className='insight-link' target="_blank" rel="noopener noreferrer" href='https://www.osservatoriodnf.it/it/home/'>Osservatorio DNF</a> ,
+        <a className='insight-link' target="_blank" rel="noopener noreferrer" href='https://dati-ustat.mur.gov.it/dataset/dati-per-bilancio-di-genere'>MUR</a>
+        </p>
+        <p style={{fontSize:"0.85rem", marginTop:"1rem"}}>Progetto realizzato con React, Vite, Three.js | Dati aggiornati al 2024</p>
     </footer>
 
   )
